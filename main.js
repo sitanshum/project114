78e635142fcd8dab765f878dd90c9be523bdf460
sitanshum <88035389+sitanshum@users.noreply.github.com>
2022-05-21 14:57:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preLoad(){
+}
+
+function setup(){
+canvas=createCanvas(300,300);
+canvas.center();
+}
+
+function draw(){
+}
+
+function take_snapshot(){
+    save("myfilterimage.png");
+}
